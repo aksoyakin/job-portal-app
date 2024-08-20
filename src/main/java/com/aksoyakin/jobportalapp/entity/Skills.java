e5ea4a6ee6601b4933db2c1e233c48cb847cbbfc
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-08-21 02:29:23 +0300

    ADDED: JobSeekerProfile, RecruiterProfile, Skills entities

---
+++ src/main/java/com/aksoyakin/jobportalapp/entity/Skills.java
@@ -0,0 +1,11 @@
+package com.aksoyakin.jobportalapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "skills")
+public class Skills {
+
+
+}
