d5173e4bf52d1603afedbfaa290dd752ded0897a
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-08-21 00:27:42 +0300

    index.html

---
+++ src/main/java/com/aksoyakin/jobportalapp/JobPortalAppApplication.java
@@ -0,0 +1,13 @@
+package com.aksoyakin.jobportalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aksoyakin/jobportalapp/JobPortalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksoyakin.jobportalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
