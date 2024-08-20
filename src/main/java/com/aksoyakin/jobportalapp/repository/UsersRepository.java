bee908b6dbb91f6fd12cc2778dee1c2b8f2ae3ff
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-08-21 01:57:08 +0300

    BUG FIXES: duplicate email ADDED: error message to the register.html

--- src/main/java/com/aksoyakin/jobportalapp/repository/UsersRepository.java
+++ src/main/java/com/aksoyakin/jobportalapp/repository/UsersRepository.java
@@ -3,5 +3,9 @@ package com.aksoyakin.jobportalapp.repository;
 import com.aksoyakin.jobportalapp.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+    Optional<Users> findByEmail(String email);
 }
