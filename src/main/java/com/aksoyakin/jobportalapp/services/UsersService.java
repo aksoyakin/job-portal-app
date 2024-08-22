ca51c88a76d5b4309922cc9e8c9daf706fef68da
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-08-22 21:06:56 +0300

    integrate dashboard with current logged in user

--- src/main/java/com/aksoyakin/jobportalapp/services/UsersService.java
+++ src/main/java/com/aksoyakin/jobportalapp/services/UsersService.java
@@ -7,6 +7,11 @@ import com.aksoyakin.jobportalapp.repository.JobSeekerProfileRepository;
 import com.aksoyakin.jobportalapp.repository.RecruiterProfileRepository;
 import com.aksoyakin.jobportalapp.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -49,5 +54,24 @@ public class UsersService {
 
     public Optional<Users> getUserByEmail(String email) {
         return usersRepository.findByEmail(email);
+    }
+
+    public Object getCurrentUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            String username = authentication.getName();
+            Users users = usersRepository.findByEmail(username).orElseThrow(() ->
+                    new UsernameNotFoundException("Could not found " + "user"));
+            int userId = users.getUserId();
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                RecruiterProfile recruiterProfile = recruiterProfileRepository.findById(userId).orElse(new RecruiterProfile());
+                return recruiterProfile;
+            }else{
+                JobSeekerProfile jobSeekerProfile = jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+                return jobSeekerProfile;
+            }
+        }
+        return null;
     }
 }
